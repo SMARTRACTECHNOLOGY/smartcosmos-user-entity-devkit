27f67a48250a75112e5f5f637b6192eba445edd1
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-08-04 23:23:37 +0200

    EnableAutoConfiguration for test

--- src/test/java/net/smartcosmos/test/TestApplicationConfiguration.java
+++ src/test/java/net/smartcosmos/test/TestApplicationConfiguration.java
@@ -1,10 +1,12 @@
 package net.smartcosmos.test;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableAutoConfiguration
 @Configuration
 public class TestApplicationConfiguration {
 
